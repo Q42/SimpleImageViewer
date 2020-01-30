6b133c37dae582599600ebac49034f9a46243872
Mathijs ‏Bernson <mathijsb@q42.nl>
2020-01-30 17:12:09 +0100

    Add support for SPM

--- Package.swift
+++ Package.swift
@@ -1,6 +1,24 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-    name: "SimpleImageViewer",
-    dependencies : []
+  name: "SimpleImageViewer",
+  platforms: [.iOS(.v10)],
+  products: [
+    .library(name: "SimpleImageViewer", targets: ["SimpleImageViewer"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "SimpleImageViewer", 
+      path: "ImageViewer"
+    ),
+    .testTarget(
+      name: "SimpleImageViewerTests", 
+      dependencies: ["SimpleImageViewer"], 
+      path: "SimpleImageViewerTests"
+    ),
+  ]
+
 )
